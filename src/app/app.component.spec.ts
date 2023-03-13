b2f05e0a88ded5f87155666e1a8934476ce79bc1
ananicole <ana.nicole02@gmail.com>
2023-03-13 18:01:57 -0300

    dist

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'profine-ana'`, () => {
+  it(`should have as title 'AnaNicoleMassaneiro.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('profine-ana');
+    expect(app.title).toEqual('AnaNicoleMassaneiro.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('profine-ana app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AnaNicoleMassaneiro.github.io app is running!');
   });
 });
